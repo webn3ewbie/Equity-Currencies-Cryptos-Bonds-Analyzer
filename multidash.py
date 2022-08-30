a9907b75f60811a802322cecc4cdba4fdca4032d
webn3ewbie <96390676+webn3ewbie@users.noreply.github.com>
2022-08-30 16:14:39 -0400

    Add files via upload

---
+++ multidash.py
@@ -0,0 +1,87 @@
+import streamlit as st
+
+def intro():
+    import streamlit as st
+
+    st.write("# Welcome to AplhaLight! ")
+    st.sidebar.success("Select a demo above.")
+
+    st.markdown(
+        """
+        AlphaLight is an open-source app framework built specifically for
+        Machine Learning and Data Science projects.
+
+        Select a dashboard from the dropdown on the left to anaylzy equities, bonds, commodities, currincies, cryptos see some examples
+        of what AlphaLight can do!
+
+        ### Want to learn more?
+
+        - Check out [streamlit.io](https://streamlit.io)
+        - Jump into our [documentation](https://docs.streamlit.io)
+        - Ask a question in our [community
+          forums](https://discuss.streamlit.io)
+
+        ### See more complex demos
+
+        - Use a neural net to [analyze the Udacity Self-driving Car Image
+          Dataset](https://github.com/streamlit/demo-self-driving)
+        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    """
+    )
+
+def price_comparison():
+    import streamlit as st
+    import pandas as pd
+    import yfinance as yf
+
+    st.title('Stock Performace Dashboard')
+    
+    st.write("Crude Oil:CL=F")                           
+    st.write("Gold:GC=F")  
+    st.write("Silver:SI=F")
+    st.write("U.S. 5 Year Treasury Yield:^FVX")
+    st.write("U.S. 10 Year Treasury Yield:^TXN")
+    st.write("U.S. 30 Year Treasury Yield:^TYX")
+    st.write("EUR/USD:EURUSD=X")
+    st.write("Dow Jones Industrial Average:^DJI")
+    st.write("S&P 500:^GSPC")
+    st.write("Nasdaq:^IXIC")
+    
+      
+      
+
+    tickers = ('TSLA','AAPL','MSFT','BTC-USD','ETH-USD','LMT','AMZN','SPY','BRK.B','META','UNH','V','NVDA','JNJ','WMT','XOM','JPM','PG','MA','GOOG','CL=F','GC=F','SI=F','^TNX','EURUSD=X','^FVX','^TYX','^RUT','^IXIC','^GSPC','^DJI')
+
+    dropdown = st.multiselect('Select your assests', tickers)
+
+    start = st.date_input('Start', value = pd.to_datetime('1970-01-01'))
+    end = st.date_input('End',value = pd.to_datetime('today'))
+
+    if len (dropdown) > 0:
+        df = yf.download(dropdown,start,end)['Adj Close']
+        st.line_chart(df)
+
+
+def asset_return():
+   import streamlit as st
+   import pandas as pd
+   import numpy as np
+   import yfinance as yf
+   
+   
+
+def data_frame_demo():
+    import streamlit as st
+    import pandas as pd
+    import numpy as np
+    import yfinance as yf
+
+page_names_to_funcs = {
+    "Home": intro,
+    "Asset Return Comparison": asset_return,
+    "Asset Price Comparison": price_comparison,
+    "DataFrame Demo": data_frame_demo
+}
+
+demo_name = st.sidebar.selectbox("Choose a dashboard", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
